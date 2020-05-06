6a8019006763938a717146c6899eab149baeb6e7
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2020-05-06 09:55:27 +0530

    upated cli

--- cli.js
+++ cli.js
@@ -22,84 +22,6 @@ program
 	.description(
 `A simple cli to get Warp+ as WireGuard configuration.
 
-<<<<<<< HEAD
-const resovle = file => path.join(CONFIG_DIR, file)
-const exists = file => fs.exists(resovle(file))
-const read = file => fs.readFile(resovle(file), 'utf-8')
-const write = (file, content) => fs.writeFile(resovle(file), content, 'utf-8')
-function sleep(ms) {
-	return new Promise(resolve => {
-		setTimeout(resolve, ms)
-	})
-}
-function printInfo(data) {
-	console.log(
-		`Your Warp credentials are located at "${resovle(
-			'data.json'
-		)}", and WireGuard connection file is "${resovle('cf-warp.conf')}".`
-	)
-	console.log(
-		`You currently have ${data.account.quota / 1000000000}GB Warp+ quota.`
-	)
-	console.log('To get your current Warp+ quota, simply run "cf-warp".')
-	console.log(
-		'To increase your Warp+ quota by 10 GB, simply run "cf-warp 10".'
-	)
-}
-async function init() {
-	console.log('Initializing Warp credentials...\n')
-	const keys = await generate()
-	const data = await register(keys)
-	await ref(data) // enable Warp+
-	const combined = Object.assign({}, keys, data, await info(data))
-	await write('data.json', JSON.stringify(combined, null, 2))
-	await write('cf-warp.conf', conf(combined))
-	printInfo(combined)
-}
-;(async () => {
-	const [dE, wE] = await Promise.all([
-		exists('data.json'),
-		exists('cf-warp.conf')
-	])
-	if (!dE) {
-		// regenerate if data.json doesn't exists
-		return init()
-	}
-	let data
-	try {
-		data = JSON.parse(await read('data.json'))
-	} catch (e) {
-		console.log(
-			'"data.json" is corrupted, all the credentials will be reset...\n'
-		)
-		return init()
-	}
-	if (!wE) {
-		console.log(
-			'"cf-warp.conf" missing but "data.json" exists, regenerating a "cf-warp.conf"...\n'
-		)
-		await write('cf-warp.conf', conf(data))
-	}
-	const n = parseInt(args[0])
-	if (!isNaN(n)) {
-		console.log(`Prepare faking Warp+ referrer for ${n} times.`)
-		for (let i = 1; i <= n; i++) {
-			if (i > 1) {
-				await sleep(20000)
-			}
-			await ref(data)
-			console.log(`#${i} fake referrer finished`)
-		}
-		console.log()
-	}
-	const newData = await info(data)
-	printInfo(newData)
-	await write(
-		'data.json',
-		JSON.stringify(Object.assign({}, data, newData), null, 2)
-	)
-})()
-=======
 Run command to generate Warp credentials and Wireguard configuration.
 Run command with configuration files in place to view Warp+ quota.
 Run command with a number to increment Warp+ quota with that many fake referrals.`
@@ -174,4 +96,3 @@ Regenerating...
 		)
 	})
 	.parse(process.argv)
->>>>>>> New CLI with optional custom config directory
